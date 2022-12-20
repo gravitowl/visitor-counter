701a9de29333c5fb2d1c4300625ebae24e1dda68
gravitowl <kjeld.berendsen@gmail.com>
2022-12-20 09:22:55 +0100

    asfefsdgsdgsd
    efwafdfeffe

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,25 +1,28 @@
 "use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var fs_1 = require("fs");
-var path_1 = require("path");
-var cors_1 = require("cors");
-var app = (0, express_1["default"])();
-app.use((0, cors_1["default"])({
-    origin: '172.104.145.28:8080'
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: '172.104.145.28:8080',
 }));
-var counter = 0;
-if (fs_1["default"].existsSync('./counter.txt')) {
-    counter = parseInt(fs_1["default"].readFileSync('./counter.txt', 'utf-8'));
+let counter = 0;
+if (fs_1.default.existsSync('./counter.txt')) {
+    counter = parseInt(fs_1.default.readFileSync('./counter.txt', 'utf-8'));
 }
-app.get('/counter', function (req, res) {
+app.get('/counter', (req, res) => {
     counter++;
-    fs_1["default"].writeFileSync('./counter.txt', String(counter));
+    fs_1.default.writeFileSync('./counter.txt', String(counter));
     return res.status(200).json({ res: counter });
 });
-app.get('/', function (req, res) {
-    res.sendFile(path_1["default"].resolve('../client/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.resolve('../client/index.html'));
 });
-app.listen(8080, function () {
+app.listen(8080, () => {
     console.log('Listening on port 8080!');
 });
